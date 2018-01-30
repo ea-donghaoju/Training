0d52f13bfa757923856d03e2f4ab6567f4398a43
ea-donghaoju <donghaoju@e-agency-china.com>
2018-01-30 11:22:44 +0800

    分离页面

---
+++ Controller/insertUserController.php
@@ -0,0 +1,16 @@
+<?php
+include('Model/searchModel.php');
+header("Content-type:text/html;charset=utf-8");
+
+
+class insertUser{
+    function insert($name){
+        $searchModel = new searchModel();
+        $insert = $searchModel->insertData($name);
+        if($insert == true){
+            return $insert;
+        }else{
+            return false;
+        }
+    }
+}
