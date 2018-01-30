1c7dd6de7104086336ab651120d9ce35a93178ac
ea-donghaoju <donghaoju@e-agency-china.com>
2018-01-30 11:20:54 +0800

    fix #1 variable camel case

--- index.php
+++ index.php
@@ -2,15 +2,13 @@
 session_start();
 header("Content-type:text/html;charset=utf-8");
 require('Model/searchModel.php');
-
+require('Controller/insertUserController.php');
 //保留上一次选择的条件值
 $_SESSION['condition'] = isset($_POST['condition'])?$_POST['condition']:"";
 $cSession = isset($_SESSION['condition'])?$_SESSION['condition']:"";
-
 //如果有报错就把下面的两个变量注释一下
 $result = "";
 $errorStr = "";
-
 
 //需要判断是insert或者是search，只使用其中一种方法
     //判断searchName是否为空，不为空进行查询
@@ -26,16 +24,10 @@ $errorStr = "";
         if($result == null){
             $errorStr = "未查询到<br/>";
         }
-    }
-    //判断insertName是否为空，不为空进行添加
-    elseif(!empty($_POST['insertName'])){
-        $insertResult = insert($_POST['insertName']);
-    }
-    //全部为空，提示输入
-    elseif(empty($_POST['searchName'])){
+    }else{
         $errorStr = "请输入内容<br/>";
     }
-require('View/index.php');
+    require('View/searchUserView.php');
 
     //-----查询方法-----
     function search($name,$searchCondition)
@@ -49,14 +41,15 @@ require('View/index.php');
             return false;;
         }
     }
+
     //-----添加方法-----
-    function insert($name)
-    {
-        $searchModel = new searchModel();
-        $insert = $searchModel->insertData($name);
-        if($insert == true){
-            return $insert;
-        }else{
-            return false;
-        }
-    }
+//    function insert($name)
+//    {
+//        $searchModel = new searchModel();
+//        $insert = $searchModel->insertData($name);
+//        if($insert == true){
+//            return $insert;
+//        }else{
+//            return false;
+//        }
+//    }
