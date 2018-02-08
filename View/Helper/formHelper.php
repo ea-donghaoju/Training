30c7048acf7e587621656df1e15757221a8f93f0
ea-donghaoju <donghaoju@e-agency-china.com>
2018-02-08 17:46:42 +0800

    fix #14   fix "formHelper.php"

--- View/Helper/formHelper.php
+++ View/Helper/formHelper.php
@@ -5,10 +5,16 @@ class formHelper
     function displayError($errorMsg)
     {
         if (!empty($errorMsg)) {
+
+            echo '<div style="font-size: 16px;color: #f00">';
+
             foreach ($errorMsg as $msg) {
                 //错误的提示
                 echo htmlspecialchars($msg) . '</br>';
             }
+
+            echo '</div>';
         }
+
     }
 }
