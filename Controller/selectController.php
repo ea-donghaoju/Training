0fa7195cf4c215ea8681e04f35c141417e8ea5c1
muzif <muzi_a@126.com>
2018-09-14 17:45:22 +0800

    #21 1-1提交

--- Controller/selectController.php
+++ Controller/selectController.php
@@ -2,10 +2,21 @@
 include('Model/databaseModel.php');
 class selectController
 {
+	//一览页面的展示
     function index(){ 
     	$databaseModel = new databaseModel();
-        $result = $databaseModel->findData(); 
+        $result = $databaseModel->select(); 
     	require("View/selectview.php");
     }
+   //数据的更改
+    // function update($id){
+    // 	$databaseModel = new databaseModel();
+    //     $result = $databaseModel->up("$id"); 
+    //     if($result){
+    //     	return json_encode('status' => 1)
+    //     }else{
+    //     	require json_encode('status' => 0)
+    //     }
+    // }
 }
 ?>
