5ead644a71b57387f00506317e3d2a750d90c02e
muzif <muzi_a@126.com>
2018-09-26 17:19:51 +0800

    #26 职位添加,基类封装第一次提交

--- Controller/departmentInsertController.php
+++ Controller/departmentInsertController.php
@@ -1,26 +1,46 @@
 <?php
 include('Model/departmentModel.php');
 class departmentInsertController{
+    //声明一个变量,存储实例化对象
+    public $departmentModel = null;
+    /**
+     * 构造函数,自动实例化DepartmentModelm模型
+     *@return object
+     */
+    public function __construct()
+    {
+        $this->departmentModel = new DepartmentModel();
+    }
+
     /**
      * 一览页添加功能
      * @return    void
      */
     public function index()
     {
-        if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+        if ($_SERVER['REQUEST_METHOD'] != 'POST') {
             require("View/departmentInsertView.php");
             return;
         }
-        $departmentName = $_POST['department_name'];
-        $_SESSION['departmentName'] = $departmentName;
-        $errorMsgArray = $this->confirmName($departmentName);
-            if ($errorMsgArray['department_name'] == null) {
-                require('View/departmentCheckView.php');
-            } else {
-                require('View/Helper/formHelper.php');
-                $formHelper = new formHelper();
-                require('View/departmentInsertView.php');
-            }
+        //插入数据时Post提交过来的数据
+        $insertName = $this->setInsertDepartmentName($_POST['department_name']);
+        $errorMsgArray = $this->confirmName($insertName);
+        if ($errorMsgArray['department_name'] != null) {
+            require('View/Helper/formHelper.php');
+            $formHelper = new formHelper();
+            require('View/departmentInsertView.php');
+            return;
+        }
+         require('View/departmentCheckView.php');
+    }
+
+    /**
+     * @param string 插入数据post提交的数据
+     * @param         [type] $name [description]
+     */
+    private function setInsertDepartmentName($name)
+    {
+        return $_SESSION['insertDepartmentName'] = $name;
     }
 
     /**
@@ -34,19 +54,19 @@ class departmentInsertController{
         $errorMsgArray['department_name'] = [];
         if (!empty($departmentName)) {
                 #去除输入的空格
-                $departmentName = trim($departmentName);
-                #输入的职位内容是否为汉字,和长度不能大于50
-                if (!$this->checkName($departmentName)) {
-                    $errorMsgArray['department_name'][] = "输入的内容只可以为汉字,字符长度不可以大于50";
-                }
-                $departmentModel = new DepartmentModel();
-                $department = $departmentModel->getDepartmentByName($departmentName);
-                if ($department->num_rows != 0) {
-                    $errorMsgArray['department_name'][] = "该职位名已经存在，请重新输入";
-                }
-            } else {
-                $errorMsgArray['department_name'][] = "输入的内容不可以为空";
+            $departmentName = trim($departmentName);
+            #输入的职位内容是否为汉字,和长度不能大于50
+            if (!$this->checkName($departmentName)) {
+                $errorMsgArray['department_name'][] = "输入的内容只可以为汉字,字符长度不可以大于50";
             }
+            $department = $this->departmentModel->getDepartmentByName($departmentName);
+            if ($department->num_rows != 0) {
+                $errorMsgArray['department_name'][] = "该职位名已经存在，请重新输入";
+            }
+            return $errorMsgArray;
+        }
+                $errorMsgArray['department_name'][] = "输入的内容不可以为空";
+
             return $errorMsgArray;
     }
 
@@ -70,18 +90,18 @@ class departmentInsertController{
      */
     public function confirm()
     {
-        if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+        if ($_SERVER['REQUEST_METHOD'] != 'POST') {
             require("View/departmentInsertView.php");
             return;
         }
-        $departmentName = $this->editDepartment($_SESSION['departmentName']);
-        if ($departmentName) {
-            $hostName = $_SERVER['HTTP_HOST'].'/dev/departmentList';
-            Header("Location: http://$hostName");
-        } else {
+        $departmentName = $this->editDepartment($_SESSION['insertDepartmentName']);
+        if (!$departmentName) {
             echo "<p style='color : red'>"."数据添加有误,请重新添加"."</p>";
             require("View/departmentInsertView.php");
+            return;
         }
+        $hostName = $_SERVER['HTTP_HOST'].'/dev/departmentList';
+        Header("Location: http://$hostName");
     }
 
     /**
@@ -91,7 +111,6 @@ class departmentInsertController{
      */
     private function editDepartment($departmentName)
     {
-        var_dump($departmentName);
         $departmentModel = new DepartmentModel();
         $departmentResult = $departmentModel -> insertDepartmentByName($departmentName);
         return $departmentResult;
